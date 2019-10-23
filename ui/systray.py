eaf316329b313ee893acac0b3c33ac1f6fac7baa
Edward-K1 <38511070+Edward-K1@users.noreply.github.com>
2019-10-23 13:53:32 +0300

    change icon file to embedded resource (#6)

--- ui/systray.py
+++ ui/systray.py
@@ -1,16 +1,16 @@
 import wx
 import wx.adv
 
-from main import ICON_PATH
+from assets.icon import DrisyIcon
+
 
 class DrisyTray(wx.adv.TaskBarIcon):
     def __init__(self, frame):
         super(DrisyTray, self).__init__()
         self.frame = frame
 
-        self.SetIcon(wx.Icon(ICON_PATH), 'Drisy')
+        self.SetIcon(DrisyIcon.GetIcon(), 'Drisy')
         self.PopupMenu = self.CreatePopupMenu()
-
 
     def CreatePopupMenu(self):
         self.menu = wx.Menu()
