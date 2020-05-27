7c7723790526d9b04678b523494f4677d61c1276
Edward-K1 <eddyk1collab@gmail.com>
2020-05-28 00:48:31 +0300

    fix topbarItem linting

--- drisy/ui/topbarItem.py
+++ drisy/ui/topbarItem.py
@@ -1,27 +1,28 @@
 import wx
 
-class TopBarItem(wx.StaticBoxSizer):
 
+class TopBarItem(wx.StaticBoxSizer):
     def __init__(self, parent, image, text, textFont):
         self.box = wx.StaticBox(parent, -1)
         super().__init__(self.box)
 
-        item = TopBarItemPanel(parent,image, text, textFont)
-        self.Add(item, 0, wx.ALL | wx.EXPAND, 0)
+        self.panel = TopBarItemPanel(parent, image, text, textFont)
+        self.Add(self.panel, 0, wx.ALL | wx.EXPAND, 0)
+
 
 class TopBarItemPanel(wx.Panel):
     def __init__(self, parent, image, text, textFont):
         super().__init__(parent, size=(200, 40))
 
-        self.image = wx.StaticBitmap(self, -1, wx.Bitmap(image),(10,5))
-        self.text = wx.StaticText(self, label=text,pos=(50,5))
+        self.image = wx.StaticBitmap(self, -1, wx.Bitmap(image), (10, 5))
+        self.text = wx.StaticText(self, label=text, pos=(50, 5))
         self.text.Font = textFont
         self.initialBackColor = self.GetBackgroundColour()
         self.initialForeColor = self.GetForegroundColour()
-        
-        self.image.Bind(wx.EVT_ENTER_WINDOW,self.OnMouseOver)
-        self.text.Bind(wx.EVT_ENTER_WINDOW,self.OnMouseOver)
-        self.Bind(wx.EVT_ENTER_WINDOW,self.OnMouseOver)
+
+        self.image.Bind(wx.EVT_ENTER_WINDOW, self.OnMouseOver)
+        self.text.Bind(wx.EVT_ENTER_WINDOW, self.OnMouseOver)
+        self.Bind(wx.EVT_ENTER_WINDOW, self.OnMouseOver)
 
         self.image.Bind(wx.EVT_LEAVE_WINDOW, self.OnMouseLeave)
         self.text.Bind(wx.EVT_LEAVE_WINDOW, self.OnMouseLeave)
@@ -38,8 +39,3 @@ class TopBarItemPanel(wx.Panel):
         self.text.SetForegroundColour(self.initialForeColor)
         self.Refresh()
         event.Skip()
-
-
-
-
-
