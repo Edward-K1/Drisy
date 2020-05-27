e37df6ee736927d9b04fca44b67e284479f847d0
Edward-K1 <eddyk1collab@gmail.com>
2020-05-27 23:22:50 +0300

    make topbarItem class-based

--- drisy/ui/topbarItem.py
+++ drisy/ui/topbarItem.py
@@ -1,16 +1,15 @@
 import wx
 
+class TopBarItem(wx.StaticBoxSizer):
 
-def TopBarItem(parent, image, text, textFont):
-    box = wx.StaticBox(parent, -1)
-    sizer = wx.StaticBoxSizer(box)
-    bmp = wx.StaticBitmap(
-        parent,
-        -1,
-        wx.Bitmap(image),
-    )
-    txt = wx.StaticText(parent, label=text)
-    txt.Font = textFont
-    sizer.Add(bmp, 0, wx.ALL | wx.EXPAND, 5)
-    sizer.Add(txt, 0, wx.ALL | wx.EXPAND, 5)
-    return sizer
+    def __init__(self, parent, image, text, textFont):
+        self.box = wx.StaticBox(parent, -1)
+        super().__init__(self.box)
+        
+        self.image = wx.StaticBitmap(parent, -1, wx.Bitmap(image))
+        self.text = wx.StaticText(parent, label=text)
+        self.text.Font = textFont
+        self.Add(self.image, 0, wx.ALL | wx.EXPAND, 5)
+        self.Add(self.text, 0, wx.ALL | wx.EXPAND, 5)
+
+        
