0872412aa6aaedd444602c71139daf3d82dbe7f3
Edward-K1 <eddyk1collab@gmail.com>
2020-06-15 00:11:27 +0300

    add mimetypes file

---
+++ drisy/core/mimetypes.py
@@ -0,0 +1,120 @@
+supported_mimetypes = {
+
+'audio': [
+    'application/vnd.google-apps.audio',
+    'audio/aac',
+    'audio/wav',
+    'audio/webm',
+    'audio/midi', 'audio/x-midi',
+    'audio/mpeg',
+    'audio/ogg',
+    'audio/opus',
+    'audio/3gpp',
+    'audio/3gpp2',
+],
+
+'video':[
+    'video/avi','video/x-msvideo',
+    'video/mpeg',
+    'video/ogg',
+    'video/webm',
+    'video/mp2t',
+    'video/3gpp',
+    'video/3gpp2'
+    'video/mp4',
+    'video/quicktime',
+    'video/x-flv',
+    'video/x-m4v',
+
+],
+
+'pdf':[
+    'application/pdf'
+],
+
+'image':[
+    'application/vnd.google-apps.photo',
+    'image/jpeg',
+    'image/bmp',
+    'image/gif',
+    'image/x-icon',
+    'image/png',
+    'image/tiff',
+    'image/svg+xml',
+],
+
+'document':[
+    'application/vnd.google-apps.document',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/vnd.oasis.opendocument.text',
+    'application/rtf',
+],
+
+'spreadsheet':[
+    'application/vnd.google-apps.spreadsheet',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.oasis.opendocument.spreadsheet',
+    'application/vnd.ms-excel',
+
+],
+
+'presentation':[
+    'application/vnd.google-apps.presentation',
+    'application/vnd.ms-powerpoint',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+
+
+],
+
+'folder':[
+    'application/vnd.google-apps.folder'
+],
+
+'archive':[
+    'application/zip',
+    'application/x-rar',
+    'application/x-bzip',
+    'application/x-bzip2',
+    'application/gzip',
+    'application/x-tar',
+    'application/x-7z-compressed',
+    
+],
+
+'code':[
+    'text/javascript',
+    'text/html'
+    'text/x-java-source'
+    'application/x-httpd-php',
+    'application/x-sh',
+    'text/x-asm',
+    'text/x-c',
+    'text/vnd.curl.scurl',
+    'text/x-fortran',
+    'text/x-pascal',
+    'text/asp',
+    'text/x-h',
+    'text/x-script.lisp',
+    'text/x-script.perl',
+    'text/x-script.python',
+    'text/x-script.zsh',
+
+],
+
+'executable':[
+    'application/java-archive',
+    'application/vnd.apple.installer+xml',
+    'application/octet-stream',
+    'application/vnd.android.package-archive',
+    'application/x-msdownload',
+    'application/x-apple-diskimage',
+    'application/hta',
+],
+
+'text':[
+    'text/plain'
+]
+
+}
