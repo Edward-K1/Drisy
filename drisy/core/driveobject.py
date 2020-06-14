0e48164a4b9e21bb20eca42c11bb524e1cce7fc9
Edward-K1 <eddyk1collab@gmail.com>
2020-06-15 00:28:12 +0300

    add mimegroup to driveobject

--- drisy/core/driveobject.py
+++ drisy/core/driveobject.py
@@ -1,4 +1,14 @@
+from .mimetypes import supported_mimetypes
+
 class DriveObject:
     def __init__(self, dict_data):
         for key in dict_data:
             setattr(self, key, dict_data[key])
+        self.mimegroup = self.assign_mimegroup(self.mimetype)
+
+    def assign_mimegroup(self, mimetype_str):
+        mimegroup = 'Unknown'
+        for group in supported_mimetypes:
+            if mimetype_str in supported_mimetypes[group]:
+                mimegroup = group.title()
+        return mimegroup
