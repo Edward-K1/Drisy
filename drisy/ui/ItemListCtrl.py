8d5c407cd6072c6d47bef78fae8979497752580e
Edward-K1 <eddyk1collab@gmail.com>
2020-06-16 20:21:33 +0300

    add DriveItemListCtrl class

---
+++ drisy/ui/ItemListCtrl.py
@@ -0,0 +1,22 @@
+import wx
+
+
+class DriveItemListCtrl(wx.ListCtrl):
+    def __init__(self, parent, imageList, imageMapIndex, columns, columnSizes):
+        super().__init__(parent, -1, style=wx.LC_REPORT | wx.LC_SORT_ASCENDING)
+        self.imageList = imageList
+        self.SetImageList(self.imageList, wx.IMAGE_LIST_SMALL)
+        self.imageIndex = imageMapIndex
+        self.columns = columns
+        self.colsizes = columnSizes
+
+        for count, colName in enumerate(columns):
+            self.InsertColumn(count, colName, width=columnSizes[count])
+
+    def InsertDriveObjects(self, objectList):
+        for count, drive_object in enumerate(objectList):
+            image_index = self.imageIndex[drive_object.mimegroup]
+            item_index = self.InsertItem(count, drive_object.filename,
+                                         image_index)
+            self.SetItem(item_index, 1, drive_object.mimegroup)
+            self.SetItem(item_index, 2, drive_object.username)
